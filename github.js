ac1a41129756028a5c3eb7f05bd2adca7ace39c4
kharne8 <cerasta81@gmail.com>
2020-11-16 01:16:02 -0600

    it now populates respos on list

--- github.js
+++ github.js
@@ -4,6 +4,7 @@ class Github {
   constructor() {
     (this.client_id = 'db267145bf6d67d1adbc'),
       (this.client_secret = 'afa1042ef595109d9dc752095dcdf0e52bb1a035');
+    (this.repos_count = 5), (this.repos_sort = 'created: asc');
   }
 
   async getUser(user) {
@@ -11,28 +12,16 @@ class Github {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}?sort=${this.repos_sort}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       profile,
+      repos,
     };
   }
 }
-
-// export default class Github {
-//   constructor() {
-//     (this.client_id = config.id), (this.client_secret = config.secret);
-//   }
-
-//   async getUser(user) {
-//     const profileResponse = await fetch(
-//       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
-//     );
-
-//     const profile = await profileResponse.json();
-
-//     return {
-//       profile,
-//     };
-//   }
-// }
